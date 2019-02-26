7191e6446270a995b7b5b6efb21bc33100262bdf
MJL <675531696@qq.com>
2019-02-26 20:21:50 +0800

    remove swift

--- ios/Classes/ClipboardManagerPlugin.m
+++ ios/Classes/ClipboardManagerPlugin.m
@@ -1,8 +1,23 @@
 #import "ClipboardManagerPlugin.h"
-#import <clipboard_manager/clipboard_manager-Swift.h>
 
 @implementation ClipboardManagerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftClipboardManagerPlugin registerWithRegistrar:registrar];
+}
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"clipboard_manager"
+            binaryMessenger:[registrar messenger]];
+  ClipboardManagerPlugin* instance = [[ClipboardManagerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"copyToClipBoard" isEqualToString:call.method]) {
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+        pasteboard.string = call.arguments;
+        result(true);
+    }
 }
 @end
